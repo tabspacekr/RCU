1be0d3cdc82ec9f18cdb6c2420314c95d73021b2
Terry Kong <82074684+tirasol@users.noreply.github.com>
2021-05-03 14:09:55 +0900

    Update hostname.sh

--- hostname/hostname.sh
+++ hostname/hostname.sh
@@ -5,5 +5,5 @@ HOSTNAME=`echo TaS-$(ifconfig eth0|grep ether |awk '{print $2}'|cut -f 4-6 -d':'
 hostnamectl set-hostname ${HOSTNAME}
 
 # 이후에 'crontab -e' 하여 마지막줄에
-# @reboot /root/hostname.sh
-# 를 추가하여준다.
+# @reboot sleep 15 && /root/hostname.sh
+# 를 추가하여준다. (재부팅시에 TaS만 표기되는 이슈가 있어 sleep 15초 추가)
